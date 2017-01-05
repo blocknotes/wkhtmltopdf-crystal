7ed4ec953be5c4b782b385b5d731278fd3ae8b19
Mattia Roccoberton <mroccoberton@mbpdev004.local>
2017-01-05 21:44:39 +0100

    Minor improvements

--- src/wkhtmltopdf/lib_wkhtmltoimage.cr
+++ src/wkhtmltopdf/lib_wkhtmltoimage.cr
@@ -18,14 +18,13 @@ lib LibWkHtmlToImage
   fun wkhtmltoimage_get_global_setting( settings : WkhtmltoimageGlobalSettings, name : LibC::Char*, value : LibC::Char*, vs : LibC::Int ): LibC::Int
 
   fun wkhtmltoimage_create_converter( settings : WkhtmltoimageGlobalSettings, data : LibC::Char* ): WkhtmltoimageConverter
-  fun wkhtmltoimage_destroy_converter( converter : WkhtmltoimageConverter )
+  fun wkhtmltoimage_destroy_converter( converter : WkhtmltoimageConverter ): Void
 
-  fun wkhtmltoimage_set_warning_callback( converter : WkhtmltoimageConverter, cb : StrCallback )
-  fun wkhtmltoimage_set_error_callback( converter : WkhtmltoimageConverter, cb : StrCallback )
-  fun wkhtmltoimage_set_phase_changed_callback( converter : WkhtmltoimageConverter, cb : VoidCallback )
-  fun wkhtmltoimage_set_progress_changed_callback( converter : WkhtmltoimageConverter, cb : IntCallback )
-  fun wkhtmltoimage_set_finished_callback( converter : WkhtmltoimageConverter, cb : IntCallback )
-
+  fun wkhtmltoimage_set_warning_callback( converter : WkhtmltoimageConverter, cb : StrCallback ): Void
+  fun wkhtmltoimage_set_error_callback( converter : WkhtmltoimageConverter, cb : StrCallback ): Void
+  fun wkhtmltoimage_set_phase_changed_callback( converter : WkhtmltoimageConverter, cb : VoidCallback ): Void
+  fun wkhtmltoimage_set_progress_changed_callback( converter : WkhtmltoimageConverter, cb : IntCallback ): Void
+  fun wkhtmltoimage_set_finished_callback( converter : WkhtmltoimageConverter, cb : IntCallback ): Void
   fun wkhtmltoimage_convert( converter : WkhtmltoimageConverter ): LibC::Int
 
   fun wkhtmltoimage_current_phase( converter : WkhtmltoimageConverter ) : LibC::Int
@@ -33,5 +32,5 @@ lib LibWkHtmlToImage
   fun wkhtmltoimage_phase_description( converter : WkhtmltoimageConverter, phase : LibC::Int ) : LibC::Char*
   fun wkhtmltoimage_progress_string( converter : WkhtmltoimageConverter ) : LibC::Char*
   fun wkhtmltoimage_http_error_code( converter : WkhtmltoimageConverter ) : LibC::Int
-  fun wkhtmltoimage_get_output( converter : WkhtmltoimageConverter, output : UInt8** ) : LibC::Long
+  fun wkhtmltoimage_get_output( converter : WkhtmltoimageConverter, output : LibC::UChar** ) : LibC::Long
 end
--- src/wkhtmltopdf/lib_wkhtmltopdf.cr
+++ src/wkhtmltopdf/lib_wkhtmltopdf.cr
@@ -14,10 +14,10 @@ lib LibWkHtmlToPdf
   fun wkhtmltopdf_version(): LibC::Char*
 
   fun wkhtmltopdf_create_global_settings(): WkhtmltopdfGlobalSettings
-  fun wkhtmltopdf_destroy_global_settings( settings : WkhtmltopdfGlobalSettings )
+  fun wkhtmltopdf_destroy_global_settings( settings : WkhtmltopdfGlobalSettings ): Void
 
   fun wkhtmltopdf_create_object_settings(): WkhtmltopdfObjectSettings
-  fun wkhtmltopdf_destroy_object_settings( settings : WkhtmltopdfObjectSettings )
+  fun wkhtmltopdf_destroy_object_settings( settings : WkhtmltopdfObjectSettings ): Void
 
   fun wkhtmltopdf_set_global_setting( settings : WkhtmltopdfGlobalSettings, name : LibC::Char*, value : LibC::Char* ): LibC::Int
   fun wkhtmltopdf_get_global_setting( settings : WkhtmltopdfGlobalSettings, name : LibC::Char*, value : LibC::Char*, vs : LibC::Int ): LibC::Int
@@ -25,21 +25,21 @@ lib LibWkHtmlToPdf
   fun wkhtmltopdf_get_object_setting( settings : WkhtmltopdfObjectSettings, name : LibC::Char*, value : LibC::Char*, vs : LibC::Int ): LibC::Int
 
   fun wkhtmltopdf_create_converter( settings : WkhtmltopdfGlobalSettings ) : WkhtmltopdfConverter
-  fun wkhtmltopdf_destroy_converter( converter : WkhtmltopdfConverter )
+  fun wkhtmltopdf_destroy_converter( converter : WkhtmltopdfConverter ): Void
 
-  fun wkhtmltopdf_set_warning_callback( converter : WkhtmltopdfConverter, cb : StrCallback )
-  fun wkhtmltopdf_set_error_callback( converter : WkhtmltopdfConverter, cb : StrCallback )
-  fun wkhtmltopdf_set_phase_changed_callback( converter : WkhtmltopdfConverter, cb : VoidCallback )
-  fun wkhtmltopdf_set_progress_changed_callback( converter : WkhtmltopdfConverter, cb : IntCallback )
-  fun wkhtmltopdf_set_finished_callback( converter : WkhtmltopdfConverter, cb : IntCallback )
+  fun wkhtmltopdf_set_warning_callback( converter : WkhtmltopdfConverter, cb : StrCallback ): Void
+  fun wkhtmltopdf_set_error_callback( converter : WkhtmltopdfConverter, cb : StrCallback ): Void
+  fun wkhtmltopdf_set_phase_changed_callback( converter : WkhtmltopdfConverter, cb : VoidCallback ): Void
+  fun wkhtmltopdf_set_progress_changed_callback( converter : WkhtmltopdfConverter, cb : IntCallback ): Void
+  fun wkhtmltopdf_set_finished_callback( converter : WkhtmltopdfConverter, cb : IntCallback ): Void
 
   fun wkhtmltopdf_convert( converter : WkhtmltopdfConverter ) : LibC::Int
-  fun wkhtmltopdf_add_object( converter : WkhtmltopdfConverter, settings : WkhtmltopdfObjectSettings, data : UInt8* )
+  fun wkhtmltopdf_add_object( converter : WkhtmltopdfConverter, settings : WkhtmltopdfObjectSettings, data : LibC::Char* ): Void
 
   fun wkhtmltopdf_current_phase( converter : WkhtmltopdfConverter ) : LibC::Int
   fun wkhtmltopdf_phase_count( converter : WkhtmltopdfConverter ) : LibC::Int
   fun wkhtmltopdf_phase_description( converter : WkhtmltopdfConverter, phase : LibC::Int ) : LibC::Char*
   fun wkhtmltopdf_progress_string( converter : WkhtmltopdfConverter ) : LibC::Char*
   fun wkhtmltopdf_http_error_code( converter : WkhtmltopdfConverter ) : LibC::Int
-  fun wkhtmltopdf_get_output( converter : WkhtmltopdfConverter, output : UInt8** ) : LibC::Long
+  fun wkhtmltopdf_get_output( converter : WkhtmltopdfConverter, output : LibC::UChar** ) : LibC::Long
 end
